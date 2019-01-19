694b5d710df581677316c0b86b196e798cd90762
Garrett Richards <garrettxaivier@gmail.com>
2019-01-18 19:23:18 -0800

    commited categories

--- server/routes/example.js
+++ server/routes/example.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const Router = express.Router()
 
-Router.get('/example', (req, res, next) => {
-  res.json({
-    example: 'example'
+const conn = require('../utils/db')
+
+Router.get('/categories', (req, res, next) => {
+  const sql = 'SELECT * FROM categories'
+  let obj = {Garrett: 'Richards'}
+
+  conn.query(sql, (error, results, fields) => {
+   obj.categories = results.filter(id => 
+      id.parent_id === null 
+    )
+
+   obj.categories.map(cat => {
+    let son = results.filter(id => {
+      if(id.parent_id === cat.id) {
+        return id
+      }
+    })
+     
+    cat.subcats = son 
+    
+    })
+    console.log(results)
+    res.json(
+      obj
+    )
   })
 })
 
